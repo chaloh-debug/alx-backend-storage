d4818c653abf616fea8c509059edcf24f33c5263
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-08-10 20:08:34 +0300

    table create fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- create table with some attributes
 -- should not fail if table exists
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT AUTOINCREMENT PRIMARY KEY NOTNULL,
     email VARCHAR(255) NOTNULL UNIQUE,
     name VARCHAR(255)
