b5caf084f103236bcd7128eee8f8fd4fd5d71cb9
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-08-10 19:59:48 +0300

    first commit

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- In or not out
 CREATE TABLE IF NOT EXISTS users (
     id INT NOTNULL AUTOINCREMENT PRIMARY KEY,
-    email VARCHAR(255),
+    email VARCHAR(255) NOTNULL UNIQUE,
     name VARCHAR(255),
     country CHAR(2) NOTNULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
