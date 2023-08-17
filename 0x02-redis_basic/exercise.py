6b1544a0fbcc46cdebf777152fae197c2ea34a0f
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-08-17 04:25:41 +0300

    update methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,16 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """ Get data from redis """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+    
+    def get_str(self, key: str) -> str:
+        """ Get a string from redis """
+        return self.get(key, lambda x: x.decode("utf-8"))
+    
+    def get_int(self, key: str) -> int:
+        """ Get an int from redis """
+        return self.get(key, lambda x: int(x.decode("utf-8")))
